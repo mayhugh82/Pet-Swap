a51a10059b6e9a74b9f867f52d21e376c98633d2
mayhugh82 <62038435+mayhugh82@users.noreply.github.com>
2020-07-16 20:23:43 -0400

    change password config to dotenv (#17)

--- config/config.js
+++ config/config.js
@@ -1,7 +1,8 @@
-{
+require('dotenv').config();
+module.exports={
   "development": {
-    "username": "root",
-    "password": "Nizlen5(",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
     "database": "petswap",
     "host": "127.0.0.1",
     "dialect": "mysql"
@@ -20,4 +21,4 @@
     "host": "127.0.0.1",
     "dialect": "mysql"
   }
-}
+};

19f63a4f2306431e97d27e67d5f2c34ba611217e
InvNO1247 <62079147+InvNO1247@users.noreply.github.com>
2020-07-18 11:51:09 -0400

    apiRoutes set up (#19)

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,51 +10,73 @@ var db = require("../models");
 
 // Routes
 // =============================================================
-module.exports = function(app) {
+module.exports = function (app) {
 
   // GET route for getting all of the posts
-  app.get("/api/owners/", function(req, res) {
-    // db.Post.findAll({})
-    //   .then(function(dbPost) {
-    //     res.json(dbPost);
-    //   });
+  app.get("/api/owners/", function (req, res) {
 
 
   });
 
   // Get route for returning matching sitters of a specific category
-  app.get("/api/sitters/state/:state/city/:city/duration/:duration/quantity/:quantity/size/:size/temperament/:temperament", function(req, res) {
+  app.get("/api/sitters/state/:state/city/:city/duration/:duration/quantity/:quantity/size/:size/temperament/:temperament", function (req, res) {
     db.Sitter.findAll({
-        where: {
-            state: req.params.state,
-            city: req.params.city,
-            duration: req.params.duration,
-            quantity: req.params.quantity,
-            size: req.params.size,
-            temperament: req.params.temperament,
-        }
-        
+      where: {
+        state: req.params.state,
+        city: req.params.city,
+        duration: req.params.duration,
+        quantity: req.params.quantity,
+        size: req.params.size,
+        temperament: req.params.temperament,
+      }
+
     })
-    .then (function(dbSitters) {
+      .then(function (dbSitters) {
         res.json(dbSitters);
-    });
+      });
   });
 
   // Get route for retrieving a single post
-  app.get("/api/posts/:id", function(req, res) {
-    // db.Post.findOne({
-    //   where: {
-    //     id: req.params.id
-    //   }
-    // })
-    //   .then(function(dbPost) {
-    //     res.json(dbPost);
-    //   });
+  app.get("/api/posts/:id", function (req, res) {
+
   });
 
-  // POST route for saving a new post
-  app.post("/api/sitter", function(req, res) {
-//    res.json(req.body);
+
+  //Logic to create owners and return matching sitters
+  app.post("/api/owner", function (req, res) {
+    //    res.json(req.body);
+    db.Owner.create({
+      name: req.body.name,
+      photo: req.body.photo,
+      city: req.body.city,
+      state: req.body.state,
+      email: req.body.email,
+      duration: req.body.duration,
+      quantity: req.body.quantity,
+      size: req.body.size,
+      temperament: req.body.temperament,
+    })
+      .then(function (dbOwner) {
+        db.Sitter.findAll({
+          where: {
+            state: dbOwner.state,
+            city: dbOwner.city,
+            duration: dbOwner.duration,
+            quantity: dbOwner.quantity,
+            size: dbOwner.size,
+            temperament: dbOwner.temperament,
+          }
+
+        })
+          .then(function (dbSitters) {
+            res.json(dbSitters);
+          });
+      });
+  });
+
+  //Logic to create sitter and return matching owners
+  app.post("/api/sitter", function (req, res) {
+    //    res.json(req.body);
     db.Sitter.create({
       name: req.body.name,
       photo: req.body.photo,
@@ -66,25 +88,27 @@ module.exports = function(app) {
       size: req.body.size,
       temperament: req.body.temperament,
     })
-      .then(function(dbSitter) {
-        res.json(dbSitter);
+      .then(function (dbSitter) {
+        db.Owner.findAll({
+          where: {
+            state: dbSitter.state,
+            city: dbSitter.city,
+            duration: dbSitter.duration,
+            quantity: dbSitter.quantity,
+            size: dbSitter.size,
+            temperament: dbSitter.temperament,
+          }
+
+        })
+          .then(function (dbOwners) {
+            res.json(dbOwners);
+          });
       });
   });
 
-  // DELETE route for deleting posts
-  app.delete("/api/posts/:id", function(req, res) {
-    // db.Post.destroy({
-    //   where: {
-    //     id: req.params.id
-    //   }
-    // })
-    //   .then(function(dbPost) {
-    //     res.json(dbPost);
-    //   });
-  });
 
   // PUT route for updating posts
-  app.put("/api/posts", function(req, res) {
+  app.put("/api/posts", function (req, res) {
     // db.Post.update(req.body,
     //   {
     //     where: {

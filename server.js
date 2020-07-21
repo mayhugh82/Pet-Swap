ae01578be3cc5e3a8f7e0762e5d4f3b7a60502c3
Dana Nobile <62401068+DanaNobile@users.noreply.github.com>
2020-07-20 20:29:43 -0400

    Updated server.js file to read app.listen and db.sequelize (#31)

--- server.js
+++ server.js
@@ -69,7 +69,9 @@ db.sequelize.sync().then(function () {
   ).then();
 
 
-  app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
+  db.sequelize.sync().then(function () {
+    app.listen(PORT, function () {
+      console.log("App listening on PORT " + PORT);
+    });
   });
 });
